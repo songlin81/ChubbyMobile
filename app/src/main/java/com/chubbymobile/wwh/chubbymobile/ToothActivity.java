d7e1a55ae379a3926ea0ad23763f7c8c50206b2f
Lin, Song <koalainuwo@hotmail.com>
2016-03-12 17:35:00 +0800

    chk1

--- app/src/main/java/com/chubbymobile/wwh/chubbymobile/ToothActivity.java
+++ app/src/main/java/com/chubbymobile/wwh/chubbymobile/ToothActivity.java
@@ -1,7 +1,11 @@
 package com.chubbymobile.wwh.chubbymobile;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,14 +13,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class ToothActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ToothActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tooth);
+        setContentView(R.layout.activity_tooth2);
+
+        AssetManager am = getAssets();
+        InputStream is = null;
+        try {
+            is = am.open("primary.jpg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
+        LinearLayout main = (LinearLayout) findViewById(R.id.root);
+        final ImageView image = new ImageView(this);
+        image.setImageBitmap(bitmap);
+        main.addView(image);
     }
 
     @Override
