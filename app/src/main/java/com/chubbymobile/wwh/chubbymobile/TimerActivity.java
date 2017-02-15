bf5f3c2337b463c9cbd162f0fb031836b060a14b
Lin, Song <koalainuwo@hotmail.com>
2017-02-15 23:43:57 +0800

    added auto camera, and timer ui

---
+++ app/src/main/java/com/chubbymobile/wwh/chubbymobile/TimerActivity.java
@@ -0,0 +1,76 @@
+package com.chubbymobile.wwh.chubbymobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class TimerActivity extends Activity {
+
+    private int mInterval = 5000; // 5 seconds by default, can be changed later
+    private Handler mHandler;
+    private int i=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        mHandler = new Handler(){
+            public void handleMessage(Message msg) {
+                final TextView tv=(TextView)findViewById(R.id.textView5);
+                Bundle b=msg.getData();
+                if(b.getString("init")!=null){
+                    tv.setText(b.getString("init").toString());
+                }
+                super.handleMessage(msg);
+            }
+        };
+        startRepeatingTask();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                updateStatus(); //this function can change value of mInterval.
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    private void updateStatus() {
+        Log.e("value",""+(i++));
+        Message m = mHandler.obtainMessage();
+        Bundle b=new Bundle();
+        b.putString("init", (""+(i++)));
+        m.setData(b);
+        mHandler.sendMessage(m);
+    }
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
+    }
+
+}
